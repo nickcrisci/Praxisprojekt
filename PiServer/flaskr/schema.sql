13dc5c714f5018db5524ac69a8db98f14fde3ca7
Nick Crisci <nick_gaetano.crisci@smail.th-koeln.de>
2022-10-27 16:36:41 +0200

    Log Route jetzt global für alle Kategorien, Zulässige Kategorien in Schema definiert

--- PiServer/flaskr/schema.sql
+++ PiServer/flaskr/schema.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS log;
 
 CREATE TABLE log (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    category TEXT NOT NULL,
+    category TEXT CHECK(category IN ('Info', 'Error', 'Debug', 'Warning', 'Other')) NOT NULL DEFAULT 'Other',
     title TEXT NOT NULL,
     body TEXT NOT NULL,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
