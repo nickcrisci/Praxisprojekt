cd587ec7f76a3a20f37619cf8f4c6c02c0f08adc
Nick Crisci <nick_gaetano.crisci@smail.th-koeln.de>
2022-11-29 16:36:54 +0100

    Automatische Log Nachricht wenn Zugriff auf sim

--- PiServer/flaskr/sim.py
+++ PiServer/flaskr/sim.py
@@ -8,6 +8,8 @@ from flaskr.db import get_db
 
 from .hardware import led, lcd
 
+from . import logging
+
 # initially set lcd to white background
 lcd.setRGB(255, 255, 255)
 
@@ -18,8 +20,10 @@ def changeLedMode():
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
         json = request.get_json()
+        logging.create_log("Info", "LED Post", f"Received a POST request to change the LED mode to {json['mode']}")
         return jsonify(led.changeLedMode(json["mode"]))
     else:
+        logging.create_log("Error", "LED Post", f"Received a POST request to change the LED mode, but the Content-Type was not application/json")
         return { "error": "Content-Type not supported" }
 
 @bp.route("/lcd", methods=["POST"])
@@ -27,6 +31,8 @@ def changeLcdText():
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
         json = request.get_json()
+        logging.create_log("Info", "LCD Post", f"Received a POST request to change the LCD text to {json['text']}")
         return jsonify(lcd.setText(json["text"]))
     else:
+        logging.create_log("Error", "LCD Post", f"Received a POST request to change the LCD text, but the Content-Type was not application/json")
         return { "error": "Content-Type not supported" }
