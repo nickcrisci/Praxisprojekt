c3bf0717c346f5088cdb7d4484165f2a4a0f9d63
Nick <nick_gaetano.crisci@smail.th-koeln.de>
2022-11-30 16:16:17 +0100

    IP angepasst
    Die IP Adresse in `ServiceBuilder.kt` muss an die IP Adresse des Gerätes angepasst werden, auf dem der Server läuft bzw. verfügbar ist.

--- AutomotiveApp/automotive/src/main/java/com/example/automotiveApp/retro/ServiceBuilder.kt
+++ AutomotiveApp/automotive/src/main/java/com/example/automotiveApp/retro/ServiceBuilder.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object ServiceBuilder {
 
     // this needs to be changed to the IP address of the device running the server
-    val baseUrl = "http://192.168.178.93:5000/"
+    val baseUrl = "http://localhost:5000/"
 
     fun getInstance(): Retrofit {
         return Retrofit.Builder().baseUrl(baseUrl)
